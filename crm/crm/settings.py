950f44ffccec79e36a148cd3aa4efa66a70b4203
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2021-04-16 08:22:57 +0100

    updated the settings file

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://crm-hero.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['crm-hero.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
