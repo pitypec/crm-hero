8d17a47fca0d2f2865458125c7f3583035eba00f
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2021-04-16 10:40:19 +0100

    changes made to register view

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,4 +1,4 @@
-
+from django.http.response import Http404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
@@ -23,8 +23,8 @@ def registerPage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            username = get_object_or_404(user, 'username')
+            form.save()
+            username = form.cleaned_data.get('username')
             messages.success(request, f"Registration for {username} successful")
             return redirect('login')
     context = {
